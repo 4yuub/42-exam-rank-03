7686b31f2d8057e46b281f4f90580e19e2724077
Ayoub Karafi <73800543+4yuub@users.noreply.github.com>
2022-02-18 13:06:08 +0100

    validated 100/100

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -1,79 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#define ARGS_ERROR "Error: argument\n"
-#define FILE_ERROR "Error: Operation file corrupted\n"
-int  w;
-int  h;
-char c;
-FILE *file;
+#include <stdbool.h>
+#define ARGS_ERR "Error: argument"
+#define FILE_ERR "Error: Operation file corrupted"
 char **zone;
+int w, h, ret;
+char c, type;
+float x_s, y_s, x_w, y_h;
+FILE *file;
 
 void putstr(char *s) {
     while (*s)
         write(1, s++, 1);
+    write(1, "\n", 1);
 }
 
-int file_err(void) {
-    putstr(FILE_ERROR);
-    return 1;
-}
-
-int draw(char type, float s_x, float s_y, float x, float y, char ch) {
-    if (type != 'R' && type != 'r')
+int draw(void) {
+    if ((type != 'r' && type != 'R') || x_w <= 0 || y_h <= 0)
         return -1;
-    if (type == 'r') {
-        // draw border
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (!(j < s_x || j > s_x + x || i < s_y || i > s_y + y)
-                    && (j - s_x < 1 || (s_x + x) - j < 1 || i - s_y < 1 || (s_y + y) - i < 1))
-                    zone[i][j] = ch;
-            }
-        }
-        return 0;
-    }
-    // draw filled
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (!(j < s_x || j > s_x + x || i < s_y || i > s_y + y))
-                zone[i][j] = ch;
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            bool cond1 = !(x < x_s || y < y_s || y > (y_s + y_h) || x > (x_s + x_w));
+            bool cond2 = x - x_s < 1 || y - y_s < 1 || (y_s + y_h) - y < 1 || (x_s + x_w) - x < 1;
+            if ((cond1) && ((cond2 && type == 'r')||type == 'R'))
+                    zone[y][x] = c;
         }
     }
     return 0;
 }
 
 int main(int ac, char **av) {
-    if (ac != 2) {
-        putstr(ARGS_ERROR);
-        return 1;
-    }
-    int ret;
+    if (ac != 2)
+        return (putstr(ARGS_ERR), 1);
     file = fopen(av[1], "r");
     if (!file)
-        return file_err();
+        return (putstr(FILE_ERR), 1);
     ret = fscanf(file, "%d %d %c\n", &w, &h, &c);
-    if (ret != 3 || w > 300 || h > 300 || w <= 0 || h <= 0)
-        return file_err();
+    if (ret != 3 || w <= 0 || h <= 0 || w > 300 || h > 300)
+        return ( putstr(FILE_ERR), 1);
     zone = calloc(h, sizeof(char *));
     for (int i = 0; i < h; i++) {
         zone[i] = calloc(w + 1, 1);
         memset(zone[i], c, w);
     }
-    float s_x, s_y, x, y;
-    char  type, ch;
-    while ((ret = fscanf(file, "%c %f %f %f %f %c\n", &type, &s_x, &s_y, &x, &y, &ch)) != -1) {
-        if (ret != 6)
-            return file_err();
-        if (draw(type, s_x, s_y, x, y, ch) == -1)
-            return file_err();
-    }
-    // print:
-    for (int i = 0; i < h; i++) {
+    while ((ret = fscanf(file, "%c %f %f %f %f %c\n", &type, &x_s, &y_s, &x_w, &y_h, &c)) != -1)
+        if (ret != 6 || draw() == -1)
+            return (putstr(FILE_ERR), 1);
+    for (int i = 0; i < h; i++)
         putstr(zone[i]);
-        putstr("\n");
-    }
-    fclose(file);
 }
